c1803ba081690f84a3361ea3bd5fe0ea09061b68
Akram Achahboun <19106572@student.hhs.nl>
2023-06-20 01:02:37 +0200

    fix: bugfix

--- demo/src/main/java/com/example/demo/ChatHandler.java
+++ demo/src/main/java/com/example/demo/ChatHandler.java
@@ -38,33 +38,10 @@ public class ChatHandler {
 
     public void nieuweChat() {
         if (buttonCount < 30) {
-            Button newButton = new Button("Chat " + (++buttonCount));
-            newButton.setStyle("-fx-background-color: transparent; -fx-text-fill: white; -fx-pref-width: 230px; -fx-pref-height: 26px; -fx-border-color: black; -fx-border-width: 1px;");
-
-            VBox newVBox = new VBox();
-            newVBox.setPrefWidth(450);
-            newVBox.setPrefHeight(520);
-            newVBox.setLayoutX(119);
-            newVBox.setLayoutY(40);
-            newVBox.setStyle("-fx-background-color: white");
-            newVBox.setVisible(false);
-
-            ScrollPane newScrollPane = new ScrollPane(newVBox);
-            newScrollPane.setPrefWidth(450);
-            newScrollPane.setPrefHeight(520);
-            newScrollPane.setLayoutX(119);
-            newScrollPane.setLayoutY(40);
-            newScrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-            newScrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-
-            TextField newTextField = new TextField();
-            newTextField.setPrefHeight(26.0);
-            newTextField.setPrefWidth(450);
-            newTextField.setLayoutX(119);
-            newTextField.setLayoutY(605);
-            newTextField.setVisible(false);
-            newTextField.setText("Stel een vraag...");
-            newTextField.setStyle("-fx-text-fill: white; -fx-background-color: grey;");
+            Button newButton = createButton();
+            VBox newVBox = createVBox();
+            ScrollPane newScrollPane = createScrollPane(newVBox);
+            TextField newTextField = createTextField(newButton, newVBox, newScrollPane);
 
             chatVBox.getChildren().add(newButton);
             buttonAnchorMap.put(newButton, new Pair<>(newVBox, newTextField));
@@ -79,51 +56,105 @@ public class ChatHandler {
 
             newButton.setOnMouseClicked(event -> {
                 if (event.getClickCount() == 2) {
-                    TextInputDialog dialog = new TextInputDialog(newButton.getText());
-                    dialog.setHeaderText("Verander het onderwerp");
-                    Optional<String> result = dialog.showAndWait();
-                    result.ifPresent(newText -> newButton.setText(newText));
+                    changeButtonSubject(newButton);
                 }
             });
 
             newTextField.setOnMouseClicked(event -> {
-                if (newTextField.getText().equals("Stel een vraag...")) {
-                    newTextField.setText("");
-                }
+                clearTextField(newTextField);
             });
 
             newTextField.setOnKeyPressed(event -> {
                 if (event.getCode() == KeyCode.ENTER) {
-                    TextArea messageTextField = new TextArea("Akram: " + newTextField.getText());
-                    messageTextField.setVisible(true);
-                    messageTextField.setWrapText(false); // Enable text wrapping
-                    messageTextField.setPrefHeight(30);
-                    messageTextField.setEditable(false);
-
-
-                    TextArea AITextField = new TextArea("Chat42: ");
-                    AITextField.setWrapText(true); // Enable text wrapping
-                    AITextField.setStyle("-fx-background-color: black;");
-                    AITextField.setVisible(true);
-                    AITextField.setWrapText(false); // Enable text wrapping
-                    AITextField.setPrefHeight(30);
-                    AITextField.setEditable(false);
-
-                    newVBox.getChildren().add(messageTextField);
-
-                    if (newTextField.getText().equals("Hoi ik ben Akram")) {
-                        AITextField.setText("Chat42: " + "Hoi Akram, ik ben Chat42");
-                    }
-
-                    newVBox.getChildren().add(AITextField);
-                    newTextField.setText("");
-
+                    processTextFieldInput(newTextField, newVBox);
                 }
             });
             anchorRechts.getChildren().add(newVBox);
-
         }
     }
+
+    private Button createButton() {
+        Button newButton = new Button("Chat " + (++buttonCount));
+        newButton.setStyle("-fx-background-color: transparent; -fx-text-fill: white; -fx-pref-width: 230px; -fx-pref-height: 26px; -fx-border-color: black; -fx-border-width: 1px;");
+        return newButton;
+    }
+
+    private VBox createVBox() {
+        VBox newVBox = new VBox();
+        newVBox.setPrefWidth(450);
+        newVBox.setPrefHeight(520);
+        newVBox.setLayoutX(119);
+        newVBox.setLayoutY(40);
+        newVBox.setStyle("-fx-background-color: white");
+        newVBox.setVisible(false);
+        return newVBox;
+    }
+
+    private ScrollPane createScrollPane(VBox content) {
+        ScrollPane newScrollPane = new ScrollPane(content);
+        newScrollPane.setPrefWidth(450);
+        newScrollPane.setPrefHeight(520);
+        newScrollPane.setLayoutX(119);
+        newScrollPane.setLayoutY(40);
+        newScrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        newScrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        return newScrollPane;
+    }
+
+    private TextField createTextField(Button button, VBox vbox, ScrollPane scrollPane) {
+        TextField newTextField = new TextField();
+        newTextField.setPrefHeight(26.0);
+        newTextField.setPrefWidth(450);
+        newTextField.setLayoutX(119);
+        newTextField.setLayoutY(605);
+        newTextField.setVisible(false);
+        newTextField.setText("Stel een vraag...");
+        newTextField.setStyle("-fx-text-fill: white; -fx-background-color: grey;");
+        return newTextField;
+    }
+
+
+
+    private void changeButtonSubject(Button button) {
+        TextInputDialog dialog = new TextInputDialog(button.getText());
+        dialog.setHeaderText("Verander het onderwerp");
+        Optional<String> result = dialog.showAndWait();
+        result.ifPresent(newText -> button.setText(newText));
+    }
+
+    private void clearTextField(TextField textField) {
+        if (textField.getText().equals("Stel een vraag...")) {
+            textField.setText("");
+        }
+    }
+
+    private void processTextFieldInput(TextField textField, VBox vbox) {
+        String inputText = textField.getText();
+        TextArea messageTextField = new TextArea("Akram: " + inputText);
+        messageTextField.setVisible(true);
+        messageTextField.setWrapText(false);
+        messageTextField.setPrefHeight(30);
+        messageTextField.setEditable(false);
+
+        TextArea AITextField = new TextArea("Chat42: ");
+        AITextField.setWrapText(true);
+        AITextField.setStyle("-fx-background-color: black;");
+        AITextField.setVisible(true);
+        AITextField.setWrapText(false);
+        AITextField.setPrefHeight(30);
+        AITextField.setEditable(false);
+
+        vbox.getChildren().add(messageTextField);
+
+        if (inputText.equals("Hoi ik ben Akram")) {
+            AITextField.setText("Chat42: " + "Hoi Akram, ik ben Chat42");
+        }
+
+        vbox.getChildren().add(AITextField);
+        textField.setText("");
+    }
+
+
 
 
     private void showAssociatedAnchorPane(Button button) {
